75ce6a3d3c17c77647053358e916cce5885c8dd2
prasannarramesh <mcmianimation@gmail.com>
2022-10-18 09:28:21 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vinoth Priyan on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
